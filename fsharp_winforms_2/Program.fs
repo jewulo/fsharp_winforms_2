b33230f5ef0a68269688307de521411610160825
Joshua Ewulo <jewulo@gmail.com>
2024-07-17 21:16:54 +0100

    Add project files.

---
+++ fsharp_winforms_2/Program.fs
@@ -0,0 +1,54 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+// https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/members/events
+
+// Implementing an Interface Event
+
+module CustomForms
+
+open System.Windows.Forms
+open System.ComponentModel
+
+type AppForm() as this =
+    inherit Form()
+
+    // Define the propertyChanged event.
+    let propertyChanged = Event<PropertyChangedEventHandler, PropertyChangedEventArgs>()
+    let mutable underlyingValue = "text0"
+
+    // Set up a click event to change the properties.
+    do
+        this.Click |> Event.add(fun evArgs -> 
+            this.Property1 <- "text2"
+            this.Property2 <- "text3")
+
+    // This property does not have a property-changed event set.
+    member val Property1 : string = "text" with get, set
+
+    // This property has a property-changed event set.
+    member this.Property2
+        with get() = underlyingValue
+        and set(newValue) = 
+            underlyingValue <- newValue
+            propertyChanged.Trigger(this, new PropertyChangedEventArgs("Property2"))
+
+    // Expose the PropertyChanged event as a first class .NET event.
+    [<CLIEvent>]
+    member this.PropertyChanged = propertyChanged.Publish
+
+    // Define the add and remove methods to implement this interface.
+    interface INotifyPropertyChanged with
+        member this.add_PropertyChanged(handler) = propertyChanged.Publish.AddHandler(handler)
+        member this.remove_PropertyChanged(handler) = propertyChanged.Publish.RemoveHandler(handler)
+
+    // This is the event-handler method.
+    member this.OnPropertyChanged(args : PropertyChangedEventArgs) = 
+        let newProperty = this.GetType().GetProperty(args.PropertyName)
+        let newValue = newProperty.GetValue(this :> obj) :?> string
+        printfn "Property {args.PropertyName} changed its value to {newValue}"
+
+// Create a form, hook up the event handler, and start the application.
+let appForm = new AppForm()
+let inpc = appForm :> INotifyPropertyChanged
+inpc.PropertyChanged.Add(appForm.OnPropertyChanged)
+Application.Run(appForm)
